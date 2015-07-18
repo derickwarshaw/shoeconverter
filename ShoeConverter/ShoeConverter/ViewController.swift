f8ced72997f352d581c9709467b878605f8ed2f0
Derick Warshaw <derickwarshaw@gmail.com>
2015-07-18 13:32:47 -0400

    completed basic functionality of project

--- ShoeConverter/ShoeConverter/ViewController.swift
+++ ShoeConverter/ShoeConverter/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mensShowSizeTextField: UITextField!
+    @IBOutlet weak var mensConvertedShoeSizeLabel: UILabel!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertButtonPressed(sender: UIButton) {
+        
+        // get string from field
+        let sizeFromTextField = mensShowSizeTextField.text
+        
+        //convert string to int
+        let numberFromTextField = sizeFromTextField.toInt()
+        
+        //unwrap this optional
+        var integerFromTextField = numberFromTextField!
+        
+        //create constant and set to 30
+        let conversionConstant = 30
+        
+        integerFromTextField += conversionConstant
+        
+        //unhide label
+        mensConvertedShoeSizeLabel.hidden = false
+        
+        // convert int to string
+        let stringWithUpdatedShoeSize = "\(integerFromTextField)" //convert integer to string
+        
+        //update label
+        mensConvertedShoeSizeLabel.text = stringWithUpdatedShoeSize
+        
+    }
 
 }
 
